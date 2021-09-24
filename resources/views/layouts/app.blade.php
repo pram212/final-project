572ffee89788a564dcad6c8c2f19161c49dcf4d2
pram212 <pramono6236@gmail.com>
2021-09-25 00:44:46 +0700

    konfigurasi templating engine

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,90 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="description" content="CodedThemes">
+    <meta name="keywords" content="flat ui, admin  Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="CodedThemes">
+    <!-- Favicon icon -->
+    <link rel="icon" href="{{asset('template/images/favicon.ico')}}" type="image/x-icon">
+    <!-- Google font--><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{asset('template/css/bootstrap/css/bootstrap.min.css')}}">
+    <!-- themify-icons line icon -->
+    <link rel="stylesheet" type="text/css" href="{{asset('template/icon/themify-icons/themify-icons.css')}}">
+    <!-- ico font -->
+    <link rel="stylesheet" type="text/css" href="{{asset('template/icon/icofont/css/icofont.css')}}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{asset('template/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('template/css/jquery.mCustomScrollbar.css')}}">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- Pre-loader start -->
+    <div class="theme-loader">
+        <div class="ball-scale">
+            <div class='contain'>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+            </div>
+        </div>
+    </div>
+    <!-- Pre-loader end -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
 
-                    </ul>
+            @include('layouts.navbar-header')
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+            <div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+                    @auth
+                    @include('layouts.sidebar-menu')
+                    @endauth
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <div class="pcoded-content">
+                        <div class="pcoded-inner-content">
+                            <div class="main-body">
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </div>
     </div>
+
+<!-- Required Jquery -->
+<script type="text/javascript" src="{{asset('template/js/jquery/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('template/js/jquery-ui/jquery-ui.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('template/js/popper.js')}}/popper.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('template/js/bootstrap/js/bootstrap.min.js')}}"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="{{asset('template/js/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="{{asset('template/js/modernizr/modernizr.js')}}"></script>
+<script type="text/javascript" src="{{asset('template/js/modernizr/css-scrollbars.js')}}"></script>
+<!-- classie js -->
+<script type="text/javascript" src="{{asset('template/js/classie/classie.js')}}"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="{{asset('template/js/script.js')}}"></script>
+<script src="{{asset('template/js/pcoded.min.js')}}"></script>
+<script src="{{asset('template/js/demo-12.js')}}"></script>
+<script src="{{asset('template/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
 </body>
+
 </html>
+
---
+++ resources/views/layouts/navbar-header.blade.php
@@ -0,0 +1,136 @@
+<nav class="navbar header-navbar pcoded-header">
+    <div class="navbar-wrapper">
+        <div class="navbar-logo">
+            @auth
+                <a class="mobile-menu" id="mobile-collapse" href="#!">
+                    <i class="ti-menu"></i>
+                </a>
+                <a class="mobile-search morphsearch-search" href="#">
+                    <i class="ti-search"></i>
+                </a>
+            @endauth
+            <a href="index.html">
+                <img class="img-fluid" src="{{asset('template/images/logo.png')}}" alt="Theme-Logo" />
+            </a>
+            <a class="mobile-options">
+                <i class="ti-more"></i>
+            </a>
+        </div>
+
+        <div class="navbar-container container-fluid">
+            @auth
+            <ul class="nav-left">
+                <li>
+                    <div class="sidebar_toggle"><a href="javascript:void(0)"><i class="ti-menu"></i></a></div>
+                </li>
+                
+                <li>
+                    <a href="#!" onclick="javascript:toggleFullScreen()">
+                        <i class="ti-fullscreen"></i>
+                    </a>
+                </li>
+            </ul>    
+            @endauth
+            <ul class="nav-right">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+
+                <li class="header-notification">
+                    <a href="#!">
+                        <i class="ti-bell"></i>
+                        <span class="badge bg-c-pink"></span>
+                    </a>
+                    <ul class="show-notification">
+                        <li>
+                            <h6>Notifications</h6>
+                            <label class="label label-danger">New</label>
+                        </li>
+                        <li>
+                            <div class="media">
+                                <img class="d-flex align-self-center" src="{{asset('template/images/user.png')}}" alt="Generic placeholder image">
+                                <div class="media-body">
+                                    <h5 class="notification-user">John Doe</h5>
+                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
+                                    <span class="notification-time">30 minutes ago</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="media">
+                                <img class="d-flex align-self-center" src="{{asset('template/images/user.png')}}" alt="Generic placeholder image">
+                                <div class="media-body">
+                                    <h5 class="notification-user">Joseph William</h5>
+                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
+                                    <span class="notification-time">30 minutes ago</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="media">
+                                <img class="d-flex align-self-center" src="{{asset('template/images/user.png')}}" alt="Generic placeholder image">
+                                <div class="media-body">
+                                    <h5 class="notification-user">Sara Soudein</h5>
+                                    <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
+                                    <span class="notification-time">30 minutes ago</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="user-profile header-notification">
+                    <a href="#!">
+                        <img src="{{asset('template/images/avatar-4.jpg')}}" class="img-radius" alt="User-Profile-Image">
+                        <span>{{Auth::user()->name}}</span>
+                        <i class="ti-angle-down"></i>
+                    </a>
+                    <ul class="show-notification profile-notification">
+                        <li>
+                            <a href="#!">
+                                <i class="ti-settings"></i> Settings
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ti-user"></i> Profile
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ti-email"></i> My Messages
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ti-lock"></i> Lock Screen
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                <i class="ti-layout-sidebar-left"></i>
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
